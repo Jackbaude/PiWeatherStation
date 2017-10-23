a5be1ff28a44c987767e499fdd43113b2dead723
Jack <Jackbaude@gmail.com>
2017-10-22 23:01:16 -0500

    Update Weather.py

--- Weather.py
+++ Weather.py
@@ -1,68 +1,46 @@
+# Jack B
+# Weather uploader 
+
+from flask import Flask, render_template, session, request
+import datetime
 app = Flask(__name__)
 import Adafruit_ADS1x15
 import time
-
-
+#40494D is gun black in html
+DEBUG = True
 
 adc = Adafruit_ADS1x15.ADS1115()
 GAIN = 1
 date = datetime.date.today()
 localtime = time.localtime(time.time())
 
+def _get_temp_voltage():
+    return  adc.read_adc(2, gain=GAIN)*4.096/32767
 
-def get_temp():
-    return  adc.read_adc(2, gain=GAIN)
+def get_tempcel():
+    tempc = (_get_temp_voltage() - 0.5) / 0.01
+    return str(float(round(tempc, 2)))
 
-def get_windspeed():
-    return  adc.read_adc(3, gain=GAIN)
+def get_tempfar():
+    tempc = (_get_temp_voltage() - 0.5) / 0.01
+    tempf = (tempc * 1.8) + 32
+    return str(float(round(tempf, 2)))
 
-def get_pressure():
-    return  adc.read_adc(1, gain=GAIN)
 
-tempv= (get_temp()*4.096/32767)
-tempc = (tempv - 0.5) / 0.01
-tempf = (tempc * 1.8) + 32
-TEMC = str(float(round(tempc, 2)))
-TEMF = str(float(round(tempf, 2)))
 
-# use for flask test
+# Tests
 @app.route('/hello')
 def hello_flask():
     return 'Hello from flask\n'
 
 
+# Live data
 
-
-# -----------------------------------------------------------------
 @app.route('/Weatherpage')
 def weatherprint():
-    return render_template('Weather1.html',date = date, TEMF = TEMF, TEMC = TEMC)
+    return render_template('Weather1.html',date = date, TEMF = get_tempfar(), TEMC = get_tempcel())
             
+
     
-    
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-if __name__ == '__main__':
-   app.run(debug=True, host="0.0.0.0")
